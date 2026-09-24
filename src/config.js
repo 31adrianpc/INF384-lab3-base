f5895599dcc2b4bc45a31e8e1d488e64991bde28
Adrian Picoy <apicoyc@gmail.com>
2026-09-24 23:22:43 +0000

    Inyeccion 1

---
+++ src/config.js
@@ -0,0 +1,6 @@
+// Credenciales del cliente de S3. INYECCION 1: credencial escrita en el codigo.
+module.exports = {
+  region: 'us-east-1',
+  accessKeyId: 'AKIAMHNJ••••••••••••',
+  secretAccessKey: 'oxnxQklqawi1h+mMQftcQkZiJ8+QZqjSzIQPUk34',
+};
